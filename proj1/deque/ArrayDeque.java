4a3ca5fcb282ee77650e3aafefa504f689da9538
pgonzalezpr <pgonzalezpr1@gmail.com>
2023-04-11 16:09:51 +0200

    added empty default method

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -84,12 +84,6 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
             return nextLast - 1;
         }
     }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
     @Override
     public int size() {
         return size;
