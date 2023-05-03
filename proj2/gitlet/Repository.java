2e7196f1b65f26cfba797fe9b78333fe02d829b4
pgonzalezpr <pgonzalezpr1@gmail.com>
2023-05-03 17:08:02 +0200

    fixed merge

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -418,6 +418,10 @@ public class Repository {
                     conflict = true;
                     continue;
                 }
+                if (!givenFiles.contains(filename)
+                        && !currentFiles.contains(filename)) {
+                    continue;
+                }
 
                 if (!givenFiles.contains(filename)
                         || !currentFiles.contains(filename)) {
